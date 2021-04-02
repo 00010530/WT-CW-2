4d36b418daa7329dcdc38ecdf32e4d35a00686bc
00010530 <student00010530@gmail.com>
2021-04-02 19:30:29 +0500

    home page and layout

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.send('To-do app')
+    res.render('home')
 })
 
 app.listen(8000, err => {
