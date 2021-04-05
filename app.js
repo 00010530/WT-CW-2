e4475e7ca786bf8d5d0b47a60a6678b455337e8f
00010530 <student00010530@gmail.com>
2021-04-05 16:52:09 +0500

    create operation for tasks

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -11,6 +14,31 @@ app.get('/', (req, res) => {
 
 app.get('/create', (req, res) => {
     res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const task = req.body.task
+    
+    if (task.trim() === '') {
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/tasks.json', (err, data) => {
+            if (err) throw err
+
+            const tasks = JSON.parse(data)
+
+            tasks.push({
+                id: id(),
+                task: task,
+            })
+
+            fs.writeFile('./data/tasks.json', JSON.stringify(tasks), err => {
+                if (err) throw err
+
+                res.render('create', { success: true })
+            })
+        })
+    }
 })
 
 const tasks = ['First task', 'Second task']
@@ -24,3 +52,7 @@ app.listen(8000, err => {
 
     console.log('Running on port 8000...')
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
